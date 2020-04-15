b2e7040ff66f216838e55dfe3c12a4c016c2710b
digvijay03 <digvijayshekhawat2001@gmail.com>
2020-04-15 23:38:01 +0530

    Initial Commit

---
+++ body/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+from .models import Post
+
+# Create your views here.
+def base(request):
+    return render(request,'body/base.html')
+
+def index(request):
+    return render(request,'body/index.html')
+
+
+
+def team(request):
+    return render(request,'body/team.html')
+def events(request):
+    context = {
+    'posts':Post.objects.all()
+    }
+    return render(request,'body/events.html',context)
+def contact(request):
+    return render(request,'body/contact.html')
