92568f217b6214d898220467df0ae76628e26bf0
digvijay03 <digvijayshekhawat2001@gmail.com>
2020-04-17 14:17:09 +0530

    font changes

--- body/urls.py
+++ body/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index,name='body-index'),
+    path('', views.home,name='home'),
 
     path('team/',views.team,name='body-team'),
     path('events/',views.events,name='body-events'),
     path('contact/',views.contact,name='contact'),
     path('gallery/',views.gallery,name='gallery'),
-    path('home/',views.home,name='home'),
+    path('index/',views.index,name='body-index'),
 
 
     ]
